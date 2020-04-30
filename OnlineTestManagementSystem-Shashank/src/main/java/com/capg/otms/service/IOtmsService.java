7fa5af041145c55954d90cbb74d5034e20863a36
SHASHANK <SHASHANK@LAPTOP-U6AKT568>
2020-04-30 08:49:14 +0530

    Otms

--- OnlineTestManagementSystem-Shashank/src/main/java/com/capg/otms/service/IOtmsService.java
+++ OnlineTestManagementSystem-Shashank/src/main/java/com/capg/otms/service/IOtmsService.java
@@ -1,10 +1,16 @@
 package com.capg.otms.service;
 
+import java.util.List;
+
+import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
+
 import com.capg.otms.entity.Questions;
 import com.capg.otms.entity.Test;
 
 public interface IOtmsService {
 	public Questions update(Questions question);
-	public Questions getResult(Test test);
+	public int getResult(Questions question);
+	public Questions addQuestions(Questions question);
+	public List<Questions>getAllQuestions();
 
 }
